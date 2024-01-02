68e4af2c4fe662d9149ebcea70ff1859f2a7103d
jiwook_han <jiwook.han.dev@gmail.com>
2024-01-03 03:49:13 +0900

    init project

---
+++ MyFirstSupabase/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  MyFirstSupabase
+//
+//  Copyright (c) 2024 z-wook. All right reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBackground
+        
+    }
+}
