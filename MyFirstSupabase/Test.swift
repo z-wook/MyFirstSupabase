97ba9b1b9b83f7ba2dadffc95257e61f7d4cb590
jiwook_han <jiwook.han.dev@gmail.com>
2024-01-04 01:55:22 +0900

    [FEAT] : ⚙️ Supabase에서 기본적인 CRUD

---
+++ MyFirstSupabase/Test.swift
@@ -0,0 +1,15 @@
+//
+//  Test.swift
+//  MyFirstSupabase
+//
+//  Copyright (c) 2024 z-wook. All right reserved.
+//
+
+import Foundation
+
+struct Test: Codable {
+    var id: Int? = nil
+    let title: String
+    let body: String
+    var created_at: Date? = nil
+}
--- MyFirstSupabase/ViewController.swift
+++ MyFirstSupabase/ViewController.swift
@@ -5,14 +5,73 @@
 //  Copyright (c) 2024 z-wook. All right reserved.
 //
 
+import Supabase
 import UIKit
 
 class ViewController: UIViewController {
     
+    let supabase = SupabaseClient(
+        supabaseURL: URL(string: PROJECT_URL)!,
+        supabaseKey: PROJECT_API_KEYS
+    )
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        configure()
+//        insertData()
+//        fetchData()
+//        upsertData()
+//        deleteData()
+    }
+}
+
+private extension ViewController {
+    func configure() {
         view.backgroundColor = .systemBackground
+    }
+    
+    func insertData() {
+        let test = Test(title: "insert", body: "insert Data")
         
+        Task {
+            try await supabase.database
+                .from("page")
+                .insert(test)
+                .execute()
+        }
+    }
+    
+    func fetchData() {
+        Task {
+            let test: [Test] = try await supabase.database
+                .from("page")
+                .select()
+                .execute()
+                .value
+            
+            print(test)
+        }
+    }
+    
+    func upsertData() {
+        let test = Test(id: 3, title: "upsert", body: "upsert Data")
+        
+        Task {
+            try await supabase.database
+                .from("page")
+                .upsert(test)
+                .execute(options:)
+        }
+    }
+    
+    func deleteData() {
+        Task {
+            try await supabase.database
+              .from("page")
+              .delete()
+              .eq("id", value: 3)
+              .execute()
+        }
     }
 }
